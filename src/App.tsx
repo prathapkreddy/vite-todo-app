ab8f6fdf5873fe6a99b0d25280702a9350bb6647
Prathap Reddy <kprathapreddy1997@gmail.com>
2024-09-18 14:32:13 -0400

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import './app.scss'
 
 function App() {
-
     return (
-        <div>
-
+        <div className={"main-wrapper"}>
+            <div className={"main-container"}>
+                <div className={'header-bar'}>TODO WEB APP</div>
+            </div>
         </div>
     )
 }
